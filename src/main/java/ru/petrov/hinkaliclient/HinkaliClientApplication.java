c5866b5a3bd15ef1a40dde3d654b6996147e7112
Aleksei Petrov <peta0982@gmail.com>
2024-05-12 12:05:52 +0500

    initialization hilkali client

---
+++ src/main/java/ru/petrov/hinkaliclient/HinkaliClientApplication.java
@@ -0,0 +1,13 @@
+package ru.petrov.hinkaliclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinkaliClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinkaliClientApplication.class, args);
+	}
+
+}
